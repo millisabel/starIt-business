9f96a1f64884eebdaa7d76a3f5cc6c6992edb796
millisabel <38396309+millisabel@users.noreply.github.com>
2021-05-20 23:37:57 +0300

    feat: added component Tabs

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import TabsResponsive from 'react-responsive-tabs';
+
+import Cabinet from "../Cabinet/Cabinet";
+import Profile from "../Profile/Profile";
+import Objects from "../Objects/Objects";
+import Feedbacks from "../Feedbacks/Feedbacks";
+
+import tabs from "./Tabs.module.css";
+import 'react-responsive-tabs/styles.css';
+
+const Tabs = () => {
+
+    const items = [
+        {
+            label: 'Personal cabinet (default)',
+            component: <Cabinet/>
+        },
+        {
+            label: 'Profile',
+            component: <Profile/>
+        },
+        {
+            label: 'My Objects',
+            component: <Objects/>
+        },
+        {
+            label: 'My Feedbacks',
+            component: <Feedbacks/>
+        },
+    ];
+
+    function getTabs() {
+        return items.map((item, index) => ({
+            title: item.label,
+            getContent: () => item.component,
+            key: index,
+            tabClassName: tabs.link,
+            panelClassName: tabs.body,
+        }));
+    }
+
+    return (
+        <TabsResponsive items={getTabs()}
+                        transform = {false}
+                        transformWidth={600}
+                        tabsWrapperClass={tabs.list}
+                        panelClassName={tabs.body}
+        />
+    );
+};
+
+export default Tabs;
